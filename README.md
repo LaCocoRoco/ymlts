2d6f313f64474f6a6b3210aaa6b946c09ee779ef
Andreas Deiring <andreas@deiring.at>
2022-03-28 04:50:50 +0200

    Folder Generator Implementation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # YAML to Typescript Generator
 <a href="https://www.npmjs.com/package/ymlts">
-  <img alt="GitHub package.json version" src="https://img.shields.io/github/package-json/v/lacocoroco/ymlts?style=for-the-badge">
+  <img alt="npm" src="https://img.shields.io/npm/v/ymlts?style=for-the-badge">
 </a>
 <a href="https://github.com/LaCocoRoco/ymlts/blob/master/LICENSE">
   <img alt="GitHub" src="https://img.shields.io/github/license/lacocoroco/ymlts.svg?style=for-the-badge" />
@@ -11,6 +11,9 @@
 
 Command Line Interface to generate Typescript from YAML File.
 Combines and simplifies the popular [quicktype](https://github.com/quicktype/quicktype) and [js-yaml](https://github.com/nodeca/js-yaml) package.
+
+# General
+Typing file extension for source and target files are optional.
 
 # Install & Usage Local 
 ```
@@ -30,60 +33,109 @@ Usage  :  ymlts source [target] [flags]
 ```
   usage : ymlts source [target] [flags]
 
-  source: source file path to .yaml or .yml
-  target: target file path to .d.ts or .ts file
+  source: source file or folder to .yaml or .yml
+  target: target file or folder to .d.ts or .ts file
   flags : -t  generate .ts instead of .d.ts
           -o  make all properties optional
+          -m  merge all files into one
           -s  silent mode
           -h  show help message
 ```
 
-# Example - Command Line Interface
+# CLI - Example File
 ```
-  cwd:    /root/repo/src
+  Example Environment
+  cwd:    /source
+  files:  /file.yaml
 ```
 ```
-  ymlts file
+  usage:  ymlts file
 
-  source: /root/repo/src/file.yaml
-  target: /root/repo/src/file.d.ts
+  source: /source/file.yaml
+  target: /source/file.d.ts
 ```
 ```
-  ymlts /usr/file -t
+  usage:  ymlts /usr/file -t
 
   source: /usr/file.yaml
   target: /usr/file.ts
 ```
 ```
-  ymlts fileYAML fileTS
+  usage:  ymlts file target
 
-  source: /root/repo/src/fileYAML.yaml
-  target: /root/repo/src/fileTS.d.ts
+  source: /source/file.yaml
+  target: /source/target.d.ts
 ```
 ```
-  ymlts template/fileYAML /usr/fileTS.d.ts -t
+  usage:  ymlts resource/file /usr/target
 
-  source: /root/repo/src/template/fileYAML.yaml
-  target: /usr/fileTS.ts
+  source: /source/resource/file.yaml
+  target: /usr/target.d.ts
 ```
 
+# CLI - Example Folder
+```
+  Example Environment
+  cwd:    /source
+  files:  /file1.yaml
+          /file2.yaml
+          /subfolder/file3.yaml
+```
+```
+  usage:  ymlts source
+
+  source: /source/file1.yaml
+          /source/file2.yaml
+          /source/subfolder/file3.yaml         
+  target: /source/file1.d.ts
+          /source/file2.d.ts
+          /source/subfolder/file3.d.ts
+```
+```
+  usage:  ymlts /usr
+
+  source: /usr/file1.yaml
+          /usr/file2.yaml
+          /usr/subfolder/file3.yaml         
+  target: /usr/file1.d.ts
+          /usr/file2.d.ts
+          /usr/subfolder/file3.d.ts
+```
+```
+  usage:  ymlts source target -t
+
+  source: /source/file1.yaml
+          /source/file2.yaml
+          /source/subfolder/file3.yaml         
+  target: /source/target/file1.ts
+          /source/target/file2.ts
+          /source/target/subfolder/file3.ts
+```
+```
+  usage:  ymlts source target/file -m
+
+  source: /source/file1.yaml
+          /source/file2.yaml
+          /source/subfolder/file3.yaml         
+  target: /source/target/file.d.ts
+```
 # Example - Input & Output
 ```yaml
-// input.yaml
+// source.yaml
 
 description:
   title: Average Temperature
   units: Degrees Fahrenheit
-  basePeriod: 1901-2000
+  basePeriod: 1900-2000
 data:
   temp:
-    value: 50.34
-    anomaly: -1.68
+    value: 50.00
+    anomaly: 1.00
 ```
 ```typescript
-// output.ts
+// target.ts
 
-export interface Source {
+export interface Target {
     description: Description;
     data:        Data;
 }
@@ -105,9 +157,9 @@ export interface Description {
 
 ```
 ```typescript
-// output.d.ts
+// target.d.ts
 
-interface Source {
+interface Target {
     description: Description;
     data:        Data;
 }
--- src/ymlts.ts
+++ src/ymlts.ts
@@ -2,8 +2,8 @@
 
 import { InputData, jsonInputForTargetLanguage, quicktype } from 'quicktype-core';
 import { load, JSON_SCHEMA } from 'js-yaml';
-import { promises, existsSync, lstatSync, readFileSync, writeFileSync } from 'fs';
-import { isAbsolute, join, parse } from 'path';
+import { readdirSync, promises, existsSync, lstatSync, readFileSync, writeFileSync } from 'fs';
+import { extname, isAbsolute, join, parse } from 'path';
 import yargs from 'yargs/yargs';
 
 /**
@@ -11,25 +11,27 @@ import yargs from 'yargs/yargs';
  */
 const cli = async () => {
   // process data
-  const argv = await yargs(process.argv.slice(2)).boolean(['t', 'h', 'o', 's']).argv;
+  const argv = await yargs(process.argv.slice(2)).boolean(['t', 'h', 'o', 's', 'm']).argv;
   const source = argv._[0] as string;
   const target = argv._[1] as string;
-  const typescript = argv.t as boolean;
+  const types = argv.t as boolean;
   const help = argv.h as boolean;
   const silent = argv.s as boolean;
   const opt = argv.o as boolean;
+  const merge = argv.m as boolean;
   const cwd = process.cwd();
 
-  // print help
   if (help) {
+    // print help
     console.log(`  usage : ymlts source [target] [flags]
 
-  source: source file path to .yaml or .yml
-  target: target file path to .d.ts or .ts file
-  flags : -t  generate .ts instead of .d.ts
-          -o  make all properties optional
-          -s  silent mode
-          -h  show help message
+    source: source file or folder to .yaml or .yml
+    target: target file or folder to .d.ts or .ts file
+    flags : -t  generate .ts instead of .d.ts
+            -o  make all properties optional
+            -m  merge all files into one
+            -s  silent mode
+            -h  show help message
     `);
     // interrupe process
     return;
@@ -39,56 +41,120 @@ const cli = async () => {
   const sourceArg = source;
   const sourceAbsolut = isAbsolute(sourceArg);
   const sourcePath = sourceAbsolut ? sourceArg : join(cwd, sourceArg);
-  const sourceDir = parse(sourcePath).dir;
-  const sourceName = parse(sourcePath).name;
-  const sourceBase = join(sourceDir, sourceName);
-  const sourceYaml = sourceBase.concat('.yaml');
-  const sourceYml = sourceBase.concat('.yml');
-  const sourceFileYaml = existsSync(sourceYaml) && lstatSync(sourceYaml).isFile();
-  const sourceFileYml = existsSync(sourceYml) && lstatSync(sourceYml).isFile();
-  const sourceFile = sourceFileYaml ? sourceYaml : sourceFileYml ? sourceYml : sourcePath;
+  const sourceIsDir = existsSync(sourcePath) && lstatSync(sourcePath).isDirectory();
+  const sourceYaml = sourcePath.concat('.yaml');
+  const sourceYml = sourcePath.concat('.yml');
+  const sourceIsYaml = existsSync(sourceYaml);
+  const sourceIsYml = existsSync(sourceYml);
+  const sourceFile = sourceIsYaml ? sourceYaml : sourceIsYml ? sourceYml : sourcePath;
+  const sourceFiles = sourceIsDir ? getYamlFiles(sourcePath) : [sourceFile];
 
+  // handle target
   const targetArg = target ? target.replace(/\.d\.ts|\.ts/g, '') : source;
   const targetAbsolut = isAbsolute(targetArg);
   const targetPath = targetAbsolut ? targetArg : join(cwd, targetArg);
-  const targetDir = parse(targetPath).dir;
-  const targetName = parse(targetPath).name;
-  const targetBase = join(targetDir, targetName);
-  const targetFile = targetBase.concat(typescript ? '.ts' : '.d.ts');
+  const targetFile = targetPath.concat(types ? '.ts' : '.d.ts');
+  const targetFiles = sourceIsDir ? sourceFiles.map(file => {
+    return file.replace(source, target).replace(/\.yml|\.yaml/g, types ? '.ts' : '.d.ts');
+  }) : [targetFile];
 
-  // print source and target
-  if (!silent) console.log('source:\t', sourceFile);
-  if (!silent) console.log('target:\t', targetFile);
+  // throw error if no source files found
+  if (!sourceIsYaml && !sourceIsYml && !sourceFiles.length) {
+    throw Error('no source files found');
+  }
 
-  // throw error file not found
-  if (!sourceFileYaml && !sourceFileYml) throw Error('source file not found');
+  if (merge) {
+    // merge types into one file
+    let samples: string[] = [];
 
-  // yaml to json
-  const file = readFileSync(sourceFile, 'utf8');
-  const doc = load(file, { schema: JSON_SCHEMA, json: true });
+    for (let i = 0; i < sourceFiles.length; i++) {
+      // print status
+      if (!silent) console.log('source:\t', sourceFiles[i]);
 
-  // json to typescript
-  const targetData = await generator(doc, targetName, typescript, opt);
+      // yaml to json
+      const yaml = readFileSync(sourceFiles[i], 'utf8');
+      const json = load(yaml, { schema: JSON_SCHEMA, json: true });
+      samples.push(JSON.stringify(json));
+    }
 
-  // write file
-  await promises.mkdir(targetDir, { recursive: true });
-  writeFileSync(targetFile, targetData);
+    // print status
+    if (!silent) console.log('target:\t', targetFile);
+
+    // json to typescript
+    const dir = parse(targetFile).dir;
+    const name = parse(targetFile.replace(/\.d\.ts|\.ts/g, '')).name;
+    const typescript = await generator(samples, name, types, opt);
+
+    // write file
+    await promises.mkdir(dir, { recursive: true });
+    writeFileSync(targetFile, typescript);
+  } else {
+    for (let i = 0; i < sourceFiles.length; i++) {
+      // print status
+      if (!silent) console.log('source:\t', sourceFiles[i]);
+      if (!silent) console.log('target:\t', targetFiles[i]);
+
+      // yaml to json
+      const yaml = readFileSync(sourceFiles[i], 'utf8');
+      const json = load(yaml, { schema: JSON_SCHEMA, json: true });
+      const samples = [JSON.stringify(json)];
+
+      // json to typescript
+      const dir = parse(targetFiles[i]).dir;
+      const name = parse(targetFiles[i].replace(/\.d\.ts|\.ts/g, '')).name;
+      const typescript = await generator(samples, name, types, opt);
+
+      // write file
+      await promises.mkdir(dir, { recursive: true });
+      writeFileSync(targetFiles[i], typescript);
+    }
+  }
 };
 
 /**
+ * Get yaml files from folder
+ * @param {string} path base folder path
+ * @returns {string[]} array of yaml files
+ */
+const getYamlFiles = (path: string): string[] => {
+  let files: string[] = [];
+
+  if (existsSync(path)) {
+    // get files from path
+    const content = readdirSync(path);
+    for (let i = 0; i < content.length; i++) {
+      const file = join(path, content[i]);
+
+      // recursive call if path is directory
+      if (lstatSync(file).isDirectory()) {
+        files = files.concat(getYamlFiles(file));
+      }
+
+      // use path if file is yaml
+      else if (extname(file).includes('.yaml')
+        || extname(file).includes('.yml')) {
+        files.push(file);
+      };
+    }
+  }
+
+  return files;
+}
+
+/**
  * parse json file to typescript
- * @param {any} doc json file
+ * @param {any} samples json file
  * @param {string} name main interface type name
  * @param {boolean} ts generate typescript file
  * @param {boolean} opt optional properties
  * @return {string} serialized typescript or typescript types file
  */
-const generator = async (doc: any, name: string, ts: boolean, opt: boolean): Promise<string> => {
+const generator = async (samples: string[], name: string, ts: boolean, opt: boolean): Promise<string> => {
   // target language
   const targetLanguage = jsonInputForTargetLanguage('ts');
   await targetLanguage.addSource({
     name: name,
-    samples: [JSON.stringify(doc)],
+    samples: samples,
   });
 
   // input data
